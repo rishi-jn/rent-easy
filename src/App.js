a86baf5ea5d3f4a555fca9db7fad7d2a3b045eab
unknown <rishijn201@gmail.com>
2024-05-23 22:23:40 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+   <>
+   <h1 className='text-2xl bg-red-500'>hello</h1>
+   </>
+  );
+}
+
+export default App;
